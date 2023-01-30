433701aa1b61b892551870b29ffd4659ce9fb957
obidekatherine <katherinedenniso@yahoo.com>
2023-01-30 11:04:50 +0100

    new commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './tailwind/output.css';
-import './index.css';
-import 'animate.css';
 import Home from "./pages/Home/Home"
 import reportWebVitals from './reportWebVitals';
 
@@ -17,3 +14,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//yarn start to start the app
